059d68f947cf372152945333e2a1670fffd3a6bc
Eivind Strand Harboe <eivisha@stud.ntnu.no>
2021-03-11 11:14:50 +0100

    Created Read.java

--- src/main/java/application/fileHandling/Read.java
+++ src/main/java/application/fileHandling/Read.java
@@ -1,4 +1,55 @@
 package application.fileHandling;
 
+import application.event.Category;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class will read from serialized objects and returns them deserialized
+ */
+
 public class Read {
+
+    public Read(){
+    }
+
+    public HashMap<Integer, Category> readCategory(){
+        HashMap<Integer, Category> categories=null;
+        try{
+            FileInputStream fileInputStream = new FileInputStream("data/categories.ser");
+            ObjectInputStream in= new ObjectInputStream(fileInputStream);
+            categories=(HashMap<Integer, Category>) in.readObject();
+            fileInputStream.close();
+            in.close();
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        catch (ClassNotFoundException c){
+            System.out.println(c.getMessage());
+        }
+        return categories;
+    }
+
+    public ArrayList<MainEvent> readEvents(){
+        ArrayList<MainEvent> events=null;
+        try{
+            FileInputStream fileInputStream = new FileInputStream("data/events.ser");
+            ObjectInputStream in= new ObjectInputStream(fileInputStream);
+            events=(ArrayList<MainEvent>) in.readObject();
+            fileInputStream.close();
+            in.close();
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        catch (ClassNotFoundException c){
+            System.out.println(c.getMessage());
+        }
+        return events;
+    }
 }
