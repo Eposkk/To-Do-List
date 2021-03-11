369a4c9d18ddb58d5b70f7053982875a357e1e23
Magnus Farstad <magnus.r.farstad@outlook.com>
2021-03-11 10:51:43 +0100

    Created class Category.java

---
+++ src/main/java/application/event/Category.java
@@ -0,0 +1,60 @@
+package application.event;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Category {
+    private final int ID;
+    private Color color;
+    private String name;
+
+    public Category(int ID, Color color, String name) {
+        this.ID = ID;
+        this.color = color;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return ID == category.ID &&
+                Objects.equals(color, category.color) &&
+                Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, color, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "ID=" + ID +
+                ", color=" + color +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
