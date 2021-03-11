e1e865d8bc7218097a2ba7f4abe33cd577506514
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-03-11 11:06:34 +0100

    Made MainEvent.java and SecondaryEvent.java

---
+++ src/main/java/application/event/MainEvent.java
@@ -0,0 +1,55 @@
+package application.event;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class MainEvent extends Event {
+    private boolean hasCategory;
+    private int categoryId;
+    private boolean HasUnderEvent;
+    private HashMap<Integer, SecondaryEvent> secondaryEvents;
+    private long serialVersionUID;
+
+    public MainEvent(int ID, LocalDate date, String name, String description, int priority, int categoryId) {
+        super(ID, date, name, description, priority);
+        this.categoryId = categoryId;
+        this.hasCategory = true;
+    }
+
+    public MainEvent(int ID, LocalDate date, String name, String description, int priority) {
+        super(ID, date, name, description, priority);
+        this.hasCategory = false;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public boolean isHasCategory() {
+        return hasCategory;
+    }
+
+    public boolean isHasUnderEvent() {
+        return HasUnderEvent;
+    }
+
+    public HashMap<Integer, SecondaryEvent> getSecondaryEvents() {
+        return secondaryEvents;
+    }
+
+
+    //TODO make method
+    public boolean addSecondaryEvent(){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MainEvent{" +
+                "hasCategory=" + hasCategory +
+                ", categoryId=" + categoryId +
+                ", HasUnderEvent=" + HasUnderEvent +
+                "} " + super.toString();
+    }
+
+}
