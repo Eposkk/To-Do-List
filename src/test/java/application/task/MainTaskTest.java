9dc9b246bd523388b8df9485f8169c3c91741d7f
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-03-18 12:44:58 +0100

    Test package

---
+++ src/test/java/application/task/MainTaskTest.java
@@ -0,0 +1,28 @@
+package application.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTaskTest {
+
+    @Test
+    void getCategoryId() {
+    }
+
+    @Test
+    void hasCategory() {
+    }
+
+    @Test
+    void hasUnderTask() {
+    }
+
+    @Test
+    void setCategoryId() {
+    }
+
+    @Test
+    void getSecondaryTasks() {
+    }
+}
