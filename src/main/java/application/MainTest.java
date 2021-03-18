afe608ab6e1ebff76eddfaa10be058f83a24f76b
Eivind Strand Harboe <eivisha@stud.ntnu.no>
2021-03-18 10:25:18 +0100

    Made app runnable

--- src/main/java/application/MainTest.java
+++ src/main/java/application/MainTest.java
@@ -1,7 +1,27 @@
 package application;
 
-public class MainTest {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class MainTest extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        var javaVersion = "12";
+        String javafxVersion = "14";
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
