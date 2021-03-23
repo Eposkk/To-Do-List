fe0074dfed9cf817d5a4ac1e7c8cefb90cc11d11
Magnus Farstad <magnus.r.farstad@outlook.com>
2021-03-23 13:57:52 +0100

    Added fxml file to today and upcoming

--- src/main/java/ntnu/team1/PrimaryController.java
+++ src/main/java/ntnu/team1/PrimaryController.java
@@ -9,4 +9,9 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private void switchToToday() throws IOException {
+        App.setRoot("today");
+    }
 }
