dc2e8403ea0010c3b5343f3e60b9f999e52fc814
Eivind Strand Harboe <eivisha@stud.ntnu.no>
2021-03-25 15:59:19 +0100

    Small update to scene loader

--- src/main/java/ntnu/team1/App.java
+++ src/main/java/ntnu/team1/App.java
@@ -17,8 +17,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("today"), 640, 480);
+        scene = new Scene(loadFXML("main"), 640, 480);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
