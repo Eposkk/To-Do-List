544964800afbc874441eef48a0a8a419f0811d2f
Simen Klemp <simenk2312@gmail.com>
2021-04-08 10:12:48 +0200

    Implemented javaDoc in Category

--- src/main/java/ntnu/team1/application/task/Category.java
+++ src/main/java/ntnu/team1/application/task/Category.java
@@ -5,37 +5,80 @@ import javafx.scene.paint.Color;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * A class that represents a category
+ * Implements serializable to be able to store the data
+ */
+
 public class Category implements Serializable {
     private final int ID;
     private Color color;
     private String name;
     private long serialVersionUID;
 
+    /**
+     *
+     * @param ID Integer that represents the category`s ID
+     * @param color Color object that represents the color of the category
+     * @param name String that represents the name of the category
+     */
     public Category(int ID, Color color, String name) {
         this.ID = ID;
         this.color = color;
         this.name = name;
     }
 
+    /**
+     *
+     * @return returns the ID of the category
+     */
+
     public int getID() {
         return ID;
     }
+
+    /**
+     *
+     * @return returns the color of the category
+     */
 
     public Color getColor() {
         return color;
     }
 
+    /**
+     *
+     * @return returns the name of the category.
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * update the color of the category
+     * @param color
+     */
 
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * updates the name of the category
+     * @param name
+     */
+
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * The method checks similarity: two Category objects are equal if their name and color are identical
+     * Identical similarity - two references to the same object
+     * Content similarity - the content of two objects is compared by category name and color
+     * @param o
+     * @return returns true if similarity, false if inequality.
+     */
 
     @Override
     public boolean equals(Object o) {
