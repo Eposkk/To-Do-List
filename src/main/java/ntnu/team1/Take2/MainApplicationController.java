0714b43ba881d201c83b69978f064eebba193907
Eivind Strand Harboe <eivisha@stud.ntnu.no>
2021-04-15 13:39:48 +0200

    Fixed menu add new task

--- src/main/java/ntnu/team1/Take2/MainApplicationController.java
+++ src/main/java/ntnu/team1/Take2/MainApplicationController.java
@@ -1,20 +1,41 @@
 package ntnu.team1.Take2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainApplicationController {
 
     @FXML
+    private MenuItem menuFileClose;
+
+    @FXML
+    private MenuItem menuEditAdd;
+
+    @FXML
+    private MenuItem menuEditEdit;
+
+    @FXML
+    private MenuItem menuEditDelete;
+
+    @FXML
+    private MenuItem menuHelpAbout;
+
+    @FXML
+    private Button add;
+
+    @FXML
     private AnchorPane view;
+
 
     public void initialize() throws IOException {
         Pane newLoadedPane = FXMLLoader.load(getClass().getResource("toDo.fxml"));
