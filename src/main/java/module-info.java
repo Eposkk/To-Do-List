bec963f2318d2b6dba5dbd0ef375f7b502ea3215
Eivind Strand Harboe <eiha0403@gmail.com>
2021-04-22 19:31:27 +0200

    Removed old master
    Merging er ikke gøy :(

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ntnu.team1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens ntnu.team1 to javafx.fxml;
-    exports ntnu.team1;
-}
--- src/main/java/ntnu/team1/application/exceptions/RemoveException.java
+++
@@ -1,7 +0,0 @@
-package ntnu.team1.application.exceptions;
-
-public class RemoveException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public RemoveException(String e){super(e);}
-}
