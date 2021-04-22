1dfe161728a0ce8d29f5180ad4c5bd89c66d0269
Eivind Strand Harboe <eiha0403@gmail.com>
2021-04-22 21:44:25 +0200

    Small text fix

--- src/main/java/ntnu/team1/mainApplication/category/package-info.java
+++ src/main/java/ntnu/team1/mainApplication/category/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains all files used for creating, editing, removing and viewing tasks
+ * Contains all files used for creating, editing, removing and viewing category
  */
 package ntnu.team1.mainApplication.category;
